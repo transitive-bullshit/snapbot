210a2d98936b602e920f10f054159fb3fcc2bd40
Travis Fischer <fisch0920@gmail.com>
2015-09-02 09:12:03 -0400

    .

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-module.exports = Platform
-
-var debug = require('debug')('snapbot:platform')
-var EventEmitter = require('events').EventEmitter
-var inherits = require('inherits')
-
-inherits(Platform, EventEmitter)
-
-/**
- * Platform
- *
- * @class
- * @param {Object} opts (currently unused)
- */
-function Platform (opts) {
-  var self = this
-  if (!(self instanceof Platform)) return new Platform(opts)
-  if (!opts) opts = {}
-}
-
-Platform.prototype.signIn = function (opts, cb) {
+module.exports = {
+  clients: {
+    TelegramClient: require('./clients/telegram-client'),
+    SnapchatClient: require('./clients/snapchat-client')
+  },
+  bots: {
+    TestBot: require('./bots/test-bot')
+  }
 }
